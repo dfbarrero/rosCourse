2a8b917f8b71a316edcaaca4df1edce4f0056583
David <david.barrero@gmail.com>
2018-02-27 18:52:57 +0100

    Typo corrected

--- 3-programming/code/odom.py
+++ 3-programming/code/odom.py
@@ -7,6 +7,6 @@ def callbackOdometry(msg):
     print msg.pose.pose
 
 if __name__ == "__main__":
-    rospy.init_node('oodometry', anonymous=True)
+    rospy.init_node('odometry', anonymous=True)
     rospy.Subscriber('odom', Odometry, callbackOdometry)
     rospy.spin()
